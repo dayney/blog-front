a6e2780fb0b2dabd8296835de2046548538db8db
dayney <krui0728@gmail.com>
2021-04-19 22:46:43 +0800

    feat::设置页面的title

--- src/router/index.js
+++ src/router/index.js
@@ -5,36 +5,57 @@ const routes = [
     path: "/",
     name: "Index",
     component: () => import("../views/index.vue"),
+    meta: {
+      title: '首页',
+    }
   },
   {
     path: "/article",
     name: "Article",
     component: () => import("../views/article.vue"),
+    meta: {
+      title: '文章列表',
+    }
   },
   {
     path: "/front",
     name: "Front",
     component: () => import("../views/front.vue"),
+    meta: {
+      title: '前端',
+    }
   },
   {
     path: "/admin",
     name: "Admin",
     component: () => import("../views/admin.vue"),
+    meta: {
+      title: '后端',
+    }
   },
   {
     path: "/about",
     name: "About",
     component: () => import("../views/about.vue"),
+    meta: {
+      title: '关于作者',
+    }
   },
   {
     path: "/404",
     name: "404",
     component: () => import("../views/404.vue"),
+    meta: {
+      title: '404',
+    }
   },
   {
     path: "/dispatch",
     name: "Dispatch",
     component: () => import("../views/dispatch.vue"),
+    meta: {
+      title: 'demo分发页面',
+    }
   },
 ];
 
@@ -44,10 +65,12 @@ const router = createRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  // ${//to and from are Route Object,next() must be called to resolve the hook}
-  // store.commit('updateLoadingStatus', {isLoading: true})
-  // console.log(to);
-  // console.log('start loading');
+  
+  // 设置page title
+  if(to.meta?.title) {
+    document.title = to.meta.title;
+  }
+
   next();
 });
 
